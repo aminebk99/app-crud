3705f38fc851cb2e975691840972d4d844dd39b5
aminebk99 <129026688+aminebk99@users.noreply.github.com>
2023-10-10 18:58:48 +0100

    update

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <h1>Product</h1>
     <table border="1px">
@@ -19,24 +21,25 @@
             <th>Delete</th>
         </tr>
         @foreach ($products as $product)
-        <tr>
+            <tr>
 
-            <td>{{$product->id}}</td>
-            <td>{{$product->name}}</td>
-            <td>{{$product->qty}}</td>
-            <td>{{$product->price}}</td>
-            <td>{{$product->description}}</td>
-            <td><a href="{{route('product.edit', ['product' => $product])}}">Edit</a></td>
-            <td>
-                <form action="{{route('product.delete', ['product'=>$product])}}" method="post">
-                    @csrf
-                    @method('delete')
-                    <input type="submit" value="Delete">
-                </form>
-            </td>
-        </tr>
-            @endforeach
+                <td>{{ $product->id }}</td>
+                <td>{{ $product->name }}</td>
+                <td>{{ $product->qty }}</td>
+                <td>{{ $product->price }}</td>
+                <td>{{ $product->description }}</td>
+                <td><a href="{{ route('product.edit', ['product' => $product]) }}">Edit</a></td>
+                <td>
+                    <form action="{{ route('product.delete', ['product' => $product]) }}" method="post">
+                        @csrf
+                        @method('delete')
+                        <input type="submit" value="Delete">
+                    </form>
+                </td>
+            </tr>
+        @endforeach
     </table>
 
 </body>
+
 </html>
